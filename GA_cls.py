de9ae12acb475818f15668df74e59a3a621ea3c7
pikeszfish <root@DeepCache.localdomain>
2015-01-30 16:41:34 +0800

    三角形变异优化

--- GA_cls.py
+++ GA_cls.py
@@ -11,7 +11,7 @@ class Color(object):
         self.r = r.randint(0, 255)
         self.g = r.randint(0, 255)
         self.b = r.randint(0, 255)
-        self.a = 100
+        self.a = r.randint(90, 110)
 
 
 class Triangle(object):
@@ -24,6 +24,17 @@ class Triangle(object):
         self.cy = r.randint(0, size[1])
         self.color = Color()
         self.img_t = None
+
+    def mutate_from(self, parent):
+        self.ax = min(max(0, parent.ax + r.randint(-15, 15)), 255)
+        self.ay = min(max(0, parent.ay + r.randint(-15, 15)), 255)
+        self.bx = min(max(0, parent.bx + r.randint(-15, 15)), 255)
+        self.by = min(max(0, parent.by + r.randint(-15, 15)), 255)
+        self.cx = min(max(0, parent.cx + r.randint(-15, 15)), 255)
+        self.cy = min(max(0, parent.cy + r.randint(-15, 15)), 255)
+        self.color.r = min(max(0, parent.color.r + r.randint(-20, 20)), 255)
+        self.color.g = min(max(0, parent.color.g + r.randint(-20, 20)), 255)
+        self.color.b = min(max(0, parent.color.b + r.randint(-20, 20)), 255)
 
     def draw_it(self, size=(256, 256)):
         self.img_t = Image.new('RGBA', size)
@@ -40,7 +51,7 @@ def mutate_or_not(rate):
 
 
 class Canvas(object):
-    mutate_rate = 0.03
+    mutate_rate = 0.02
     size = (256, 256)
     size_1 = (255, 255)
     target_pixels = []
@@ -62,11 +73,23 @@ class Canvas(object):
         self.target_pixels = t
 
     def mutate_from_parent(self, parent):
+        flag = False
         for triangle in parent.triangles:
             t = triangle
             if mutate_or_not(self.mutate_rate):
-                t = Triangle()
+                flag = True
+                a = Triangle()
+                a.mutate_from(t)
+                self.triangles.append(a)
+                continue
             self.triangles.append(t)
+        # OK We need mutate at least one at one time. It's dirty, but hmmm
+        if not flag:
+            self.triangles.pop()
+            t = parent.triangles[r.randint(0, len(parent.triangles)-1)]
+            a = Triangle()
+            a.mutate_from(t)
+            self.triangles.append(a)
 
     def calc_match_rate(self):
         if self.match_rate > 0:
@@ -77,7 +100,7 @@ class Canvas(object):
         draw.polygon([(0, 0), (0, 255), (255, 255), (255, 0)], fill = (0, 0, 0, 0))
         for triangle in self.triangles:
             self.img = Image.alpha_composite(self.img, triangle.img_t or triangle.draw_it(self.size))
-        pixels = [self.img.getpixel((x, y)) for x in range(0, self.size[0], 4) for y in range(0, self.size[1], 4)]
+        pixels = [self.img.getpixel((x, y)) for x in range(0, self.size[0], 2) for y in range(0, self.size[1], 2)]
         for i in range(0, min(len(pixels), len(self.target_pixels))):
             delta_red   = pixels[i][0] - self.target_pixels[i][0]
             delta_green = pixels[i][1] - self.target_pixels[i][1]
@@ -87,18 +110,18 @@ class Canvas(object):
                                delta_blue  * delta_blue
 
     def draw_it(self, i):
-        self.img.save("/home/conplat/GA_engine/dd_%d_n%d_%d.png" % (i, len(self.triangles), int(self.mutate_rate * 100)))
+        self.img.save("/home/conplat/GA_engine/bb_%d_n%d_%d_128.png" % (i, len(self.triangles), int(self.mutate_rate * 100)))
 
 
 def main():
-    img_path = "/home/conplat/GA_engine/dd.png"
-    img = Image.open(img_path).convert('RGBA')
+    img_path = "/home/conplat/GA_engine/bb.png"
+    img = Image.open(img_path).resize((256, 256)).convert('RGBA')
     size = (256, 256)
     size_1 = (255, 255)
-    target_img = [img.getpixel((x, y)) for x in range(0, size[0], 4) for y in range(0, size[1], 4)]
+    target_img = [img.getpixel((x, y)) for x in range(0, size[0], 2) for y in range(0, size[1], 2)]
     Canvas.target_pixels = target_img
     parent = Canvas()
-    parent.add_triangles(200)
+    parent.add_triangles(90)
     i = 0
     while True:
         child = Canvas()
